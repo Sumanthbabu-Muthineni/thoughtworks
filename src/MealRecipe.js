f0fbfb576c21ddaacf5b6ae2dea5bd62cb8684bf
Sumanthbabu Muthineni <118552809+Sumanthbabu-Muthineni@users.noreply.github.com>
2024-09-16 11:55:24 +0000

    added wrapping changes

--- src/MealRecipe.js
+++ src/MealRecipe.js
@@ -1,17 +1,29 @@
 import React from "react";
+import styles from "./styles.css";
 
-const Recipe = ({ title, category, area, image, instructions }) => (
-  <div className="recipe">
-    <h3 className="meal-head">{title}</h3>
-    <p>
-      <strong>Category:</strong> {category}
-    </p>
-    <p>
-      <strong>Area:</strong> {area}
-    </p>
-    <img src={image} className="image-size" alt={title} />
-    <p className="recipe-details">{instructions}</p>
-  </div>
-);
+const Recipe = ({ title, category, area, image, instructions }) => {
+  const [showDetails, setShowDetails] = useState(false);
 
+  return (
+    <div className="recipe-card">
+      <h3 className="meal-head">{title}</h3>
+      <p>
+        <strong>Category:</strong> {category}
+      </p>
+      <p>
+        <strong>Area:</strong> {area}
+      </p>
+      <img src={image} className="image-size" alt={title} />
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "Hide mealDetails" : "ShowMealDetails"}
+      </button>
+      {showDetails && (
+        <div className="recipe-instructions">
+          <h3>Instructions:</h3>
+          <p>{instructions}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 export default Recipe;
